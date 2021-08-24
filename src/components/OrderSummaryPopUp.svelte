e7924f29c48695cd976cb6c06d316ba749cea57f
ZeynebKoc <koczeyneb@gmail.com>
2021-08-24 15:08:39 +0300

    Edit OrderSummaryPopUp's css

--- src/components/OrderSummaryPopUp.svelte
+++ src/components/OrderSummaryPopUp.svelte
@@ -11,7 +11,6 @@
 
     export let showOrderSummaryPopUp;
     export let closeOrderSummaryPopUp;
-
 </script>
 
 {#if showOrderSummaryPopUp}
@@ -25,24 +24,28 @@
             <h2 class="order-summary-title">Order Summary</h2>
         </div>
 
-        <OrderSummaryContent
-            bind:valuePreferences
-            bind:valueBeanType
-            bind:valueQuantity
-            bind:valueGrindOption
-            bind:valueDeliveries
-        />
+        <div class="order-summary-chapter">
+            <div class="order-summary-content">
+                <OrderSummaryContent
+                    bind:valuePreferences
+                    bind:valueBeanType
+                    bind:valueQuantity
+                    bind:valueGrindOption
+                    bind:valueDeliveries
+                />
+            </div>
 
-        <p class="order-summary-remind">
-            Is this correct? You can proceed to checkout or go back to<br />
-            plan selection if something is off. Subscription discount<br />
-            codes can also be redeemed at the checkout.
-        </p>
+            <p class="order-summary-remind">
+                Is this correct? You can proceed to checkout or go back to<br />
+                plan selection if something is off. Subscription discount<br />
+                codes can also be redeemed at the checkout.
+            </p>
 
-        <div class="order-summary-payment">
-            <div class="order-summary-total">$14.00/ mo</div>
-            <div class="order-summary-btn">
-                <Button buttonName="Checkout" />
+            <div class="order-summary-payment">
+                <div class="order-summary-total">$14.00/ mo</div>
+                <div class="order-summary-btn">
+                    <Button buttonName="Checkout" />
+                </div>
             </div>
         </div>
     </main>
@@ -57,24 +60,29 @@
         z-index: 2;
         height: 4136px;
         width: 1440px;
-        margin: 0px;
+        margin: -113px;
     }
     main {
         width: 540px;
-        height: 597px;
-        margin-left: 450px;
-        margin-top: 214px;
         background: #fefcf7;
         border-radius: 8px;
         position: fixed;
         z-index: 3;
+        margin: -113px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
     .order-summary-header {
         background: #333d4b;
         border-radius: 8px 8px 0px 0px;
         width: 540px;
         height: 136px;
-        margin-top: -33px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
     }
     .order-summary-title {
         font-family: "Fraunces", serif;
@@ -83,28 +91,23 @@
         font-size: 40px;
         line-height: 48px;
         color: #ffffff;
-        padding-top: 48px;
-        padding-left: 57px;
+        padding: 48px 148px 40px 56px;
     }
-    /* .section-order-summary-content {
-        height: 160px;
+    .order-summary-chapter {
+        width: 461px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 58px 0px 57px 0px;
+    }
+    .order-summary-content {
         width: 428px;
-        margin-left: 56px;
-        margin-top: 65px;
-    }
-    .section-order-summary-text {
-        font-family: "Fraunces", serif;
-        font-style: normal;
-        font-weight: 900;
-        font-size: 24px;
-        line-height: 40px;
         color: #83888f;
-        margin-top: -7px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
     }
-    .order-summary-green-text {
-        display: inline;
-        color: #0e8784;
-    } */
     .order-summary-remind {
         font-family: "Barlow", sans-serif;
         font-style: normal;
@@ -114,14 +117,20 @@
         color: #333d4b;
         mix-blend-mode: normal;
         opacity: 0.8;
-        margin-left: 56px;
+        width: 428px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
         margin-top: 6px;
     }
     .order-summary-payment {
-        margin-left: 56px;
-        margin-top: 58px;
+        width: 428px;
         display: flex;
         flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 31px;
     }
     .order-summary-total {
         font-family: "Fraunces", serif;
@@ -130,9 +139,5 @@
         font-size: 32px;
         line-height: 36px;
         color: #333d4b;
-    }
-    .order-summary-btn {
-        margin-left: -66px;
-        margin-top: -66px;
     }
 </style>
